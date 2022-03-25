7e51ce07cc3b841230947e8fbbbc2961bb961cae
Aluno <stephanny.nascimento08@gmail.com>
2022-03-25 20:13:35 -0300

    Primeira Parte Projeto Agenda

---
+++ 24_03_2022/app/src/main/java/com/example/aula_2022_03_24/FormularioPersonagemActivity.java
@@ -0,0 +1,4 @@
+package com.example.aula_2022_03_24;
+
+public class FormularioPersonagemActivity {
+}
---
+++ 24_03_2022/app/src/main/java/com/example/aula_2022_03_24/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aula_2022_03_24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personagem);
+    }
+}
